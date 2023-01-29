9a6b153565710aa292d82738cf42901f83dba90b
Kipeno Lemayian <lemakipeno@gmail.com>
2023-01-29 14:03:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import './App.css';
+import { Main } from "./pages/main"
+import { Login } from "./pages/login"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element = {<Main />}></Route>
+          <Route path="/login" element = {<Login />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
